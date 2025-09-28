99d06f72f7654e4c4b2fc9fd8ab9d942a5fddd5f
LighT of HeaveN <jeric.ggonzales.dev@gmail.com>
2025-09-28 18:58:53 +0800

    a lot to add

---
+++ src/weather-app.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react"
+function weather(){
+    return(<div>
+        <button>
+            <h1>what the heck </h1>
+        </button>
+    </div>)
+}
+export default weather
